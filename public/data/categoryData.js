1992fe0f25c1924b7e6440636a0c124ad5784632
Harsh Boghara <harshboghra2004@gmail.com>
2025-03-06 17:50:08 +0530

    data moved to public folder

---
+++ public/data/categoryData.js
@@ -0,0 +1,17 @@
+export const categories = [
+    { id: "action", name: "Action", image: "/images/action.jpg" },
+    {
+        id: "sports_and_Racing",
+        name: "Sports & Racing",
+        image: "/images/sports_and_racing.png",
+    },
+    { id: "adventure", name: "Adventure", image: "/images/adventure.jpg" },
+    { id: "strategy", name: "Strategy", image: "/images/strategy.png" },
+    { id: "merge", name: "Merge", image: "/images/merge.png" },
+    {
+        id: "puzzle_and_Logic",
+        name: "Puzzle & Logic",
+        image: "/images/puzzle_and_logic.jpg",
+    },
+    { id: "arcade", name: "Arcade", image: "/images/arcade.jpg" },
+];
