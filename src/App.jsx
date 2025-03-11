a713f426481f0bd54c1e873e4943cde8d39ff42a
Harsh Boghara <harshboghra2004@gmail.com>
2025-03-11 16:19:01 +0530

    search route removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import classes from "./App.module.css";
 import HomePage from "./pages/Home/HomePage";
@@ -9,9 +8,6 @@ import LoginPage from "./pages/Auth/LoginPage";
 import SignUpPage from "./pages/Auth/SignUpPage";
 import ForgotPasswordPage from "./pages/Admin/ForgetPasswordPage";
 import Footer from "./components/UI/Footer";
-import SearchPage from "./pages/Search/SearchPage";
-import { games } from "../public/data/gameData";
-import { categories } from "../public/data/categoryData";
 
 const App = () => {
     return (
@@ -25,12 +21,6 @@ const App = () => {
                 <Route
                     path="/forget-password"
                     element={<ForgotPasswordPage />}
-                />
-                <Route
-                    path="/search"
-                    element={
-                        <SearchPage games={games} categories={categories} />
-                    }
                 />
                 <Route path="*" element={<NotFoundPage />} />
             </Routes>
